9ea4703bc0432cbd87ee5e58b6d439fc1de847e1
Matheus Verri <74548310+matheusverri@users.noreply.github.com>
2023-12-22 16:01:31 +0000

    API creation

---
+++ soy-crawler.rb
@@ -0,0 +1,29 @@
+require 'nokogiri'
+require 'open-uri'
+require 'sinatra'
+
+set :protection, except: :json_csrf
+
+doc = Nokogiri::HTML(URI.open('https://br.investing.com/commodities/us-soybeans'))
+
+price_and_variation = doc.css('div.gap-x-4').css('.font-bold')
+
+price = price_and_variation.first.content
+variation = price_and_variation[1].content
+
+get '/' do
+  content_type :json
+  { Price: price,
+    Variation: variation
+  }.to_json
+end
+
+get '/price' do
+  content_type :json
+  { Price: price }.to_json
+end
+
+get '/variation' do
+  content_type :json
+  { Variation: variation }.to_json
+end
