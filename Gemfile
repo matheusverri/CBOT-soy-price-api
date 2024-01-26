df9c6b6eafcc7e279317fb1aed313458d06fb512
Matheus Verri <74548310+matheusverri@users.noreply.github.com>
2024-01-26 14:59:32 +0000

    Rufus Scheduler adicionado para testes

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
 gem 'nokogiri'
 gem 'sinatra'
 gem 'unicorn'
+gem 'rufus-scheduler'
--- soy-crawler.rb
+++ soy-crawler.rb
@@ -1,8 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
 require 'sinatra'
+require 'rufus-scheduler'
 
 set :protection, except: :json_csrf
+
+scheduler = Rufus::Scheduler.new
+
+scheduler.every '3s' do
+  puts 'Hello... Rufus'
+end
 
 doc = Nokogiri::HTML(URI.open('https://br.investing.com/commodities/us-soybeans'))
 
