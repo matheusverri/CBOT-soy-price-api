7539efe267c0861cdc3da3c518a6e7a681a0507a
Matheus Verri <74548310+matheusverri@users.noreply.github.com>
2024-02-05 16:44:36 +0000

    Santos adicionado

--- soy-crawler.rb
+++ soy-crawler.rb
@@ -10,6 +10,7 @@ scheduler = Rufus::Scheduler.new
 price = nil
 variation = nil
 paranagua_price = nil
+santos_price = nil
 
 scheduler.every '5m' do
   #CBOT
@@ -22,8 +23,14 @@ scheduler.every '5m' do
   paranagua_doc = Nokogiri::HTML(URI.open('https://www.noticiasagricolas.com.br/cotacoes/soja'))
   paranagua_price = paranagua_doc.css('div.cotacao').css('table.cot-fisicas').css('td')[1].content
 
+  # SANTOS
+  santos_doc = Nokogiri::HTML(URI.open('https://www.noticiasagricolas.com.br/cotacoes/soja/soja-mercado-fisico-sindicatos-e-cooperativas'))
+  santos_price = santos_doc.css('tbody').first.css('tr').last.css('td')[1].content
+
   puts 'Preço Atualizado - ' + Time.now.to_s
 end
+
+scheduler.join
 
 get '/' do
   content_type :json
@@ -50,6 +57,15 @@ get '/paranagua-price' do
   {
     Location: 'Paranaguá',
     Paranagua_price: paranagua_price,
+    unit: 'R$/SC60kg'
+  }.to_json
+end
+
+get '/santos-price' do
+  content_type :json
+  {
+    Location: 'Santos/SP',
+    santos_price: santos_price,
     unit: 'R$/SC60kg'
   }.to_json
 end
